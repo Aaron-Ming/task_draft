3f971458362b871682678ddb3cc98eefd6ebbcc9
Aaron_Ming <aaron_ming@outlook.com>
2016-05-09 17:21:36 +0800

    create model

--- actual_08/08/zuoye.py
+++ actual_08/08/zuoye.py
@@ -1,5 +1,38 @@
 #encoding=utf-8
 #
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,session
 
-print 'hello world'
+
+app = Flask(__name__)
+app.secret_key = '1234567890!@#$%^&*()'
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return 'Hello World.'
+
+@app.route('/log_page')
+def log_page():
+    pass
+
+@app.route('/user_page')
+def user_page():
+    pass
+
+@app.route('/update_user')
+def update_user():
+    pass
+
+@app.route('/add_user')
+def add_user():
+    pass
+
+@app.route('/del_user')
+def del_user():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
