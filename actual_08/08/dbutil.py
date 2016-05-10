be230454b558be52472724dd070d98a6c1769776
Aaron_Ming <aaron_ming@outlook.com>
2016-05-10 17:57:03 +0800

    commit from Ming

--- actual_08/08/dbutil.py
+++ actual_08/08/dbutil.py
@@ -1,3 +1,27 @@
 #encoding=utf-8
 #
+import MySQLdb as mysql
+con = None
+cursor = None
 
+def connect():
+    global con
+    global cursor
+    con = mysql.connect(db='mingguangzhen', host='180.153.191.128', user='reboot', passwd='reboot123')
+    con.autocommit(True)
+    cursor = con.cursor()
+
+
+def execute(sql):
+    try:
+        cursor.execute(sql)
+    except Exception:
+        try:
+            con.close()
+            cursor.close()
+        except Exception:
+            pass
+        print 'db connect again.'
+        connect()
+        cursor.execute(sql)
+        
--- actual_08/08/zuoye.py
+++ actual_08/08/zuoye.py
@@ -1,22 +1,39 @@
 #encoding=utf-8
 #
-from flask import Flask,render_template,redirect,session
+from flask import Flask,render_template,redirect,session,url_for,request
 
 
 app = Flask(__name__)
 app.secret_key = '1234567890!@#$%^&*()'
 
+@app.route('/')
+def index():
+    return render_template('layout_page.html')
+
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return 'Hello World.'
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        user = request.form.get('user')
+        pwd = request.form.get('pwd')
+        if user not in get_user.auth_info():
+            return 'user not exists.'
+        elif get_user.auth_info()[user] == pwd:
+            session['user'] = user           
+            return redirect('/home')
+        else:
+            return 'password error.'
 
 @app.route('/log_page')
 def log_page():
-    pass
+    page_title = 'Log-Page'
+    return render_template('log_page.html', page_title=page_title)
 
 @app.route('/user_page')
 def user_page():
-    pass
+    page_title = 'User management'
+    return render_template('user_page.html', page_title=page_title)
 
 @app.route('/update_user')
 def update_user():
