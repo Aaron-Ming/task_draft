f854ea09f2c2f0b34a1c1467b4dc6ffa95f0a026
Aaron_Ming <aaron_ming@outlook.com>
2016-05-11 17:25:29 +0800

    Commit from Ming 20150511

--- actual_08/08/zuoye.py
+++ actual_08/08/zuoye.py
@@ -1,7 +1,7 @@
 #encoding=utf-8
 #
 from flask import Flask,render_template,redirect,session,url_for,request
-
+import dbutil
 
 app = Flask(__name__)
 app.secret_key = '1234567890!@#$%^&*()'
@@ -17,39 +17,57 @@ def login():
     elif request.method == 'POST':
         user = request.form.get('user')
         pwd = request.form.get('pwd')
-        if user not in get_user.auth_info():
+        if user not in dbutil.auth_info():
             return 'user not exists.'
-        elif get_user.auth_info()[user] == pwd:
-            session['user'] = user           
-            return redirect('/home')
+        elif dbutil.auth_info()[user] == pwd:
+            session['user'] = user
+            return redirect('/user_page')
         else:
             return 'password error.'
 
 @app.route('/log_page')
 def log_page():
-    page_title = 'Log-Page'
-    return render_template('log_page.html', page_title=page_title)
+    page = request.args.get('page',0)
+    cont = request.args.get('cont',10)
+    page = int(page)
+    cont = int(cont)
+    if 100%cont:
+        all_pages = (100/cont)
+    else:
+        all_pages = 100/cont-1
+    logs = dbutil.log_info(page*cont,cont)
+    return render_template('log_page.html', logs=logs,page=page,all_pages=all_pages)
 
 @app.route('/user_page')
 def user_page():
-    page_title = 'User management'
-    return render_template('user_page.html', page_title=page_title)
+
+    user_info = dbutil.user_info()
+    return render_template('user_page.html', user_info=user_info)
 
 @app.route('/update_user')
 def update_user():
-    pass
+    uppwd = request.args.get('uppwd')
+    id = request.args.get('id')
+    dbutil.update_user(uppwd,id)
+    return redirect('/user_page')
 
-@app.route('/add_user')
+@app.route('/add_user', methods=['POST'])
 def add_user():
-    pass
+    user = request.form.get('user')
+    pwd = request.form.get('pwd')
+    dbutil.ins_user(user=user,pwd=pwd)
+    return redirect('/user_page')
 
 @app.route('/del_user')
 def del_user():
-    pass
+    id = request.args.get('id')
+    dbutil.del_user(id)
+    return redirect('/user_page')
 
 @app.route('/logout')
 def logout():
-    pass
+    session.pop('user', None)
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
