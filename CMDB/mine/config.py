f9945cf3f8e5265efd9b36efd86aeecad337f15a
Aaron_Ming <aaron_ming@outlook.com>
2016-06-21 17:24:39 +0800

    add jiaja2 template

---
+++ CMDB/mine/config.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+
+
+# 数据库配置
+
+db_config = {
+	'host':'101.200.125.9',
+	'user':'mingguangzhen',
+	'passwd':"123456",
+	'db':'cmdb'
+}
+
+
+page_config = {
+    "brand_name":'51Reboot',
+    'title':'hello reboot',
+    "favicon":'https://pic1.zhimg.com/6d660dd4156c64bfad13ff97d79c2f98_l.jpg',
+    "menu":[
+        {
+            # user配置最好不要修改，是和登陆认证相关的，直接在下面加配置即可
+            "name": 'user',
+            "title": '用户管理',
+            "data": [{
+                "name": 'username',
+                "title": '用户名'
+            },{
+                "name":'password',
+                "title":'密码'
+            }]
+        },
+
+	{
+         "name": 'host',
+         "title": '服务器',
+         "data": [{
+             "name": 'jifang',
+             "title": '机房'
+         },{
+              "name": 'jigui',
+             "title": '机柜'
+         },{
+              "name": 'SN',
+             "title": 'SN'
+         },{
+              "name": 'wanip',
+             "title": '公网IP'
+         },{
+              "name": 'lanip',
+             "title": '内网IP'
+         },{
+              "name": 'iloip',
+             "title": '管理IP'
+         },{
+              "name": 'prog',
+             "title": '项目'
+         },{
+              "name": 'app',
+             "title": '应用'
+             }]
+         }
+
+
+   ]
+     }
--- CMDB/mine/flask_web.py
+++ CMDB/mine/flask_web.py
@@ -1,23 +1,38 @@
 #encoding=utf-8
 #
 from flask import Flask,render_template
+from config import db_config,page_config
+from dbutil.dbutil import DB
+import json
+import sys,os
 
-import sys
+# print db_config
 
-
-reload(sys)
-sys.setdefaultencoding("utf-8")
+db = DB(host=db_config['host'], mysql_user=db_config['user'], mysql_pass=db_config['passwd'], mysql_db=db_config['db'])
 app = Flask(__name__)
-app.secret_key = '1234567890qwertyuiopasdfghjkl'
-
+app.secret_key = os.urandom(24)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
     return '这是登录页面'
 
+@app.route('/vm_assets_data')
+def vm_assets_data():
+    sql = 'select * from vm_assets'
+    # print db.execute(sql)
+    # return 'hh'
+    res = json.dumps(db.execute(sql))
+    return res
+
+
 @app.route('/vm_assets')
 def vm_assets():
-    return render_template('vm_assets.html')
+    sql = 'select * from vm_assets'
+    # print db.execute(sql)
+    # return 'hh'
+    # res = json.dumps(db.execute(sql))
+    res = db.execute(sql)
+    return render_template('vm_assets.html',get_vmassets=res)
 
 @app.route('/layout')
 def layout():
@@ -26,3 +41,6 @@ def layout():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2898, debug=True)
+
+
+
