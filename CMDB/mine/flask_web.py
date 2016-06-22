d8ed455baee49000e5dc8e552bbb616e19b343e8
Aaron_Ming <aaron_ming@outlook.com>
2016-06-22 17:30:35 +0800

    sdf

--- CMDB/mine/flask_web.py
+++ CMDB/mine/flask_web.py
@@ -1,6 +1,6 @@
 #encoding=utf-8
 #
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from config import db_config,page_config
 from dbutil.dbutil import DB
 import json
@@ -16,23 +16,64 @@ app.secret_key = os.urandom(24)
 def login():
     return '这是登录页面'
 
+@app.route('/user')
+def user():
+    return '这个是用户目录'
+
 @app.route('/vm_assets_data')
 def vm_assets_data():
     sql = 'select * from vm_assets'
-    # print db.execute(sql)
-    # return 'hh'
     res = json.dumps(db.execute(sql))
     return res
-
 
 @app.route('/vm_assets')
 def vm_assets():
     sql = 'select * from vm_assets'
-    # print db.execute(sql)
-    # return 'hh'
-    # res = json.dumps(db.execute(sql))
     res = db.execute(sql)
     return render_template('vm_assets.html',get_vmassets=res)
+
+@app.route('/vm_assets/add', methods=['POST'])
+def vm_assets_add():
+    ip_addr = request.form.get('ip_addr',None)
+    app_name = request.form.get('app_name',None)
+    hostname = request.form.get('hostname',None)
+    vc_name = request.form.get('vc_name',None)
+    cpu = request.form.get('cpu',None)
+    memory = request.form.get('memory',None)
+    disk = request.form.get('disk',None)
+    os = request.form.get('os',None)
+    status = request.form.get('status',None)
+    office_name = request.form.get('office_name',None)
+    office_contact = request.form.get('office_contact',None)
+    office_phone = request.form.get('office_phone',None)
+    object_contact = request.form.get('object_contact',None)
+    object_phone = request.form.get('object_phone',None)
+    create_date = request.form.get('create_date',None)
+    end_date = request.form.get('end_date',None)
+    notes = request.form.get('notes',None)
+
+    sql = 'insert into vm_assets(ip_addr,app_name,hostname,vc_name,cpu,memory,disk,os,status,office_name,office_contact,office_phone,object_contact,object_phone,create_date,end_date,notes) values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (ip_addr,app_name,hostname,vc_name,cpu,memory,disk,os,status,office_name,office_contact,office_phone,object_contact,object_phone,create_date,end_date,notes)
+    try:
+        db.execute(sql)
+    except:
+        return 'error'
+    else:
+        return 'ok'
+
+@app.route('/vm_assets/delete', methods=['POST'])
+def vm_assets_del():
+    id = request.form.get('id', None)
+    sql = 'delete from vm_assets where id=%s' % (id)
+    try:
+        db.execute(sql)
+    except:
+        return 'error'
+    else:
+        return 'ok'
+
+@app.route('/vm_assets/update')
+def vm_assets_update():
+    return True
 
 @app.route('/layout')
 def layout():
@@ -40,7 +81,7 @@ def layout():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2898, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
 
 
 
--- actual_08/11_my/zuoye.py
+++ actual_08/11_my/zuoye.py
@@ -174,4 +174,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
